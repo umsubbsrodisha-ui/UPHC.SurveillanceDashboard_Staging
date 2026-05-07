e421e98b13e0f3c2effcf4a7523ea31259ffca94
umsubbsrodisha-ui <umsubbsrodisha@gmail.com>
2026-05-07 11:33:37 +0530

    Worked on alerts

--- Models/UPHC.cs
+++ Models/UPHC.cs
@@ -11,17 +11,17 @@
     //}
 
 
-    public class UPHC
-    {
-        public int Id { get; set; }
+    //public class UPHC
+    //{
+    //    public int Id { get; set; }
 
-        public string Name { get; set; } = "";
-        public string Location { get; set; } = "";
+    //    public string Name { get; set; } = "";
+    //    public string Location { get; set; } = "";
 
-        // 🔗 One UPHC → Many Cases
-        public ICollection<CaseRecord> CaseRecords { get; set; } = new List<CaseRecord>();
+    //    // 🔗 One UPHC → Many Cases
+    //    public ICollection<CaseRecord> CaseRecords { get; set; } = new List<CaseRecord>();
 
-       public ICollection<Notification> notifications { get; set; }
+    //   public ICollection<Notification> notifications { get; set; }
 
-    }
+    //}
 }
